c8e95eeab893ee0e9366433b4bba63eb01a78426
Manuel Chavez <manuel1995_01@outlook.com>
2023-02-15 13:23:08 -0500

    Finishing logic and bindings

--- MVVM/Models/BMI.cs
+++ MVVM/Models/BMI.cs
@@ -1,5 +1,8 @@
-﻿namespace BMI_MAUI.MVVM.Models
+﻿using PropertyChanged;
+
+namespace BMI_MAUI.MVVM.Models
 {
+    [AddINotifyPropertyChangedInterface]
     public class BMI
     {
         private float result;
@@ -11,6 +14,45 @@
             get
             {
                 return ((Weight / Height) / Height) * 10000;
+            }
+        }
+        public string ResultText 
+        { 
+            get
+            {
+                string template = "BMI: #";
+                if (Result <= 16)
+                {
+                    return template.Replace("#", "Severe Thinness");
+                }
+                else if (Result > 16 && Result <= 17)
+                {
+                    return template.Replace("#", "Moderate Thinness");
+                }
+                else if (Result > 17 && Result <= 18.5)
+                {
+                    return template.Replace("#", "Mild Thinness");
+                }
+                else if (Result > 18.5 && Result <= 25)
+                {
+                    return template.Replace("#", "Normal");
+                }
+                else if (Result > 25 && Result <= 30)
+                {
+                    return template.Replace("#", "Overweight");
+                }
+                else if (Result > 30 && Result <= 35)
+                {
+                    return template.Replace("#", "Obese Class I");
+                }
+                else if (Result > 35 && Result <= 40)
+                {
+                    return template.Replace("#", "Obese Class II");
+                }
+                else
+                {
+                    return template.Replace("#", "Obese Class III");
+                }
             }
         }
     }
